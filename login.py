b274d5269a1ab2834067de75982133fa3d212166
warhead2002 <daanish303@gmail.com>
2022-07-26 18:04:39 +0530

    Merge branch 'main' of https://github.com/Runtime-Terror-NFC/need-for-code-35

--- login.py
+++ login.py
@@ -4,11 +4,11 @@ con = sqlite3.connect('login.db')
 cursor = con.cursor()
 
 try:
-    com = "CREATE TABLE login(email NVARCHAR(255),name VARCHAR(255),password VARCHAR(255))"
+    com = "CREATE TABLE login(email NVARCHAR(255),name VARCHAR(255),password VARCHAR(255),position VARCHAR(255))"
     cursor.execute(com)
 except:
     exit
-com1  ="INSERT INTO login VALUES('daanish303@gmail.com','daanish peerkhan','daani')"
+com1  ="INSERT INTO login VALUES('daanish303@gmail.com','daanish peerkhan','daani','student')"
 cursor.execute(com1)
 con.commit()
 con.close()
--- main.py
+++ main.py
@@ -1,28 +1,55 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,session,redirect
 import sqlite3
 
 app = Flask('__name__')
-app.config['SECRET_KEY'] = 'secretsecret'
+app.secret_key = 'secretsecret'
+temp = 0
 
 @app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        connection = sqlite3.connect('login.db')
         email = request.form.get('email')
         password = request.form.get('password')
-        return email+" "+password
+        connection = sqlite3.Connection('login.db')
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM login")
+        result = cursor.fetchall()
+        for i in range(len(result)):
+            db_email = result[i][0]
+            db_password = result[i][2]
+            user_type = result[i][3]
+            if email == db_email and password == db_password:
+                temp = 1
+        if temp == 1:
+            session['login'] = 1
+            if user_type == 'student':
+                return redirect('/student')
+            elif user_type == 'teacher':
+                return redirect('/teacher')
+
+        else:
+            session['login']=0
+            return render_template('login.html',flag = "Invalid Login")
+
     return render_template('login.html')
 
 @app.route('/student',methods=["POST","GET"])
 def student_page():
-    return 'student-page.html'
+    if session['login']==1:
+        return render_template('student-page.html')
+    else:
+        redirect('/login')
 
 @app.route('/teacher')
 def teacher_page():
     return render_template('teacher-page.html')
+
+@app.route('/admin')
+def admin():
+    return render_template('admin-page.html')
 
 app.run()
